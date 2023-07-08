4d3426ca3fb75f08689a6bc6af6605b3b0a13ae0
Volodymyr Kushnir <volodumur.kushnir@gmail.com>
2023-07-08 18:35:07 +0300

    Newform reestr

---
+++ js-index.js
@@ -0,0 +1,10 @@
+
+
+
+
+document.getElementById('top-menu-user-btn').addEventListener('click', function() {
+    var menu = document.getElementById('top-menu-user');
+    menu.classList.toggle('visible');
+  });
+  
+
