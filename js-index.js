5d32be0298dbf29c11ca98e2f83e605a2d2599ba
Volodymyr Kushnir <volodumur.kushnir@gmail.com>
2023-07-09 19:05:16 +0300

    імя юзераo

--- js-index.js
+++ js-index.js
@@ -12,7 +12,22 @@ const urlParams = new URLSearchParams(window.location.search);
 const jsonDataParam = urlParams.get('data');
 const nameDataParam = urlParams.get('nameData');
 
-// Перевірка наявності даних у URL-параметрі
+// // Перевірка наявності даних у URL-параметрі
+// if (jsonDataParam) {
+//   // Розпакування JSON-рядка в об'єкт
+//   const registrationData = JSON.parse(decodeURIComponent(jsonDataParam));
+
+//   // Отримання значень полів з об'єкта
+//   const firstName = registrationData.firstName;
+//   const lastName = registrationData.lastName;
+//   const phoneNumber = registrationData.phoneNumber;
+//   const email = registrationData.email;
+
+//   // Використання отриманих даних
+//   // Наприклад, встановлення значень полів на сторінці або виведення їх у консоль
+//   console.log(firstName, lastName, email, phoneNumber );
+// }
+
 if (jsonDataParam) {
   // Розпакування JSON-рядка в об'єкт
   const registrationData = JSON.parse(decodeURIComponent(jsonDataParam));
@@ -20,11 +35,12 @@ if (jsonDataParam) {
   // Отримання значень полів з об'єкта
   const firstName = registrationData.firstName;
   const lastName = registrationData.lastName;
-  const phoneNumber = registrationData.phoneNumber;
-  const email = registrationData.email;
 
-  // Використання отриманих даних
-  // Наприклад, встановлення значень полів на сторінці або виведення їх у консоль
-  console.log(firstName, lastName, email, phoneNumber );
+  // Встановлення значень полів на сторінці
+  const nameElement = document.getElementById('Name-user');
+  nameElement.innerText = `${firstName} ${lastName}`;
+
+  // Зміна стилю на видимий
+  const userElement = document.querySelector('.user');
+  userElement.style.display = 'block';
 }
-
