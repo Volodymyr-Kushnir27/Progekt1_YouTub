9361f55b54deabd00173e84825caf1b96b91958e
Volodymyr Kushnir <volodumur.kushnir@gmail.com>
2023-07-11 01:39:38 +0300

    Settingo

--- js-index.js
+++ js-index.js
@@ -7,40 +7,45 @@ document.getElementById('top-menu-user-btn').addEventListener('click', function(
 
 
 
-// Отримання URL-параметра з даними
-const urlParams = new URLSearchParams(window.location.search);
-const jsonDataParam = urlParams.get('data');
-const nameDataParam = urlParams.get('nameData');
+const jsonDataParam = localStorage.getItem('registrationData');
 
-// // Перевірка наявності даних у URL-параметрі
-// if (jsonDataParam) {
-//   // Розпакування JSON-рядка в об'єкт
-//   const registrationData = JSON.parse(decodeURIComponent(jsonDataParam));
-
-//   // Отримання значень полів з об'єкта
-//   const firstName = registrationData.firstName;
-//   const lastName = registrationData.lastName;
-//   const phoneNumber = registrationData.phoneNumber;
-//   const email = registrationData.email;
-
-//   // Використання отриманих даних
-//   // Наприклад, встановлення значень полів на сторінці або виведення їх у консоль
-//   console.log(firstName, lastName, email, phoneNumber );
-// }
 
 if (jsonDataParam) {
   // Розпакування JSON-рядка в об'єкт
-  const registrationData = JSON.parse(decodeURIComponent(jsonDataParam));
+  const registrationData = JSON.parse(jsonDataParam);
 
   // Отримання значень полів з об'єкта
   const firstName = registrationData.firstName;
   const lastName = registrationData.lastName;
+  const fphoneNumber = registrationData.phoneNumber;
+  const email = registrationData.email
 
   // Встановлення значень полів на сторінці
   const nameElement = document.getElementById('Name-user');
   nameElement.innerText = `${firstName} ${lastName}`;
 
+
+
   // Зміна стилю на видимий
   const userElement = document.querySelector('.user');
   userElement.style.display = 'block';
 }
+
+
+if (jsonDataParam) {
+
+  const registrationData = JSON.parse(jsonDataParam);
+
+  const firstName = registrationData.firstName;
+  const lastName = registrationData.lastName;
+  const fphoneNumber = registrationData.phoneNumber;
+  const email = registrationData.email
+
+const nameUser = document.getElementById('name');
+nameUser.innerText = `${firstName}`;
+const surnameUser = document.getElementById('surname');
+surnameUser.innerText = `${lastName}`;
+const phoneUser = document.getElementById('phone');
+phoneUser.innerText = `${fphoneNumber}`;
+const emailsUser = document.getElementById('emails');
+emailsUser.innerText = `${email}`; };
--- js-reestrs.js
+++ js-reestrs.js
@@ -6,9 +6,9 @@ function register() {
   const email = document.getElementById('email').value;
 
   // Перевірка правильності номеру телефону
-  const phoneRegex = /^\d{9}$/;
+  const phoneRegex = /^\d{10}$/;
   if (!phoneRegex.test(phoneNumber)) {
-    document.getElementById('nofon').textContent = 'Введіть коректний номер телефону (9 цифр)!';
+    document.getElementById('nofon').textContent = 'Введіть коректний номер телефону (10 цифр)!';
     document.getElementById('nofon').style.display = 'block';
     return;
   }
@@ -20,28 +20,37 @@ function register() {
     document.getElementById('noemail').style.display = 'block';
     return;
   }
-
   // Створення об'єкта з даними реєстрації
   const registrationData = {
+    firstName,
+    lastName, };
+  
+  const nameData = {
+    
     firstName,
     lastName,
     phoneNumber,
     email
   };
-  console.log(registrationData ) ;
 
-  const jsonData = JSON.stringify(registrationData);
-  // Перехід на іншу сторінку з передачею даних через URL-параметр
-  if (phoneNumber && email) {
-    window.location.href = `https://volodymyr-kushnir27.github.io/Progekt1_YouTub/index.html?data=${encodeURIComponent(jsonData)}`;
-  }
+
+  console.log(nameData) ;
+
+
+  localStorage.setItem('registrationData', JSON.stringify(nameData);
+  localStorage.setItem('registrationData', JSON.stringify(registrationData));
+
+  // Перехід на іншу сторінку
+  // if (phoneNumber && email) {
+  //   window.location.href = 'https://volodymyr-kushnir27.github.io/Progekt1_YouTub/index.html';
+   //}
 }
 
 // Обмеження введення символів в поле номеру телефону
 const phoneNumberInput = document.getElementById('phoneNumber');
 phoneNumberInput.addEventListener('input', function(event) {
   const input = event.target.value;
-  event.target.value = input.replace(/\D/g, '').slice(0, 9);
+  event.target.value = input.replace(/\D/g, '').slice(0, 10);
 });
 
 
